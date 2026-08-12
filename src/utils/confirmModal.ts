d2bf1c48ac808b3d79d5223b64a396b4a94ed9d6
weshell <weshell@foxmail.com>
2026-08-12 13:06:21 +0800

    fix: add return undefined to satisfy promise/always-return lint rule

--- src/utils/confirmModal.ts
+++ src/utils/confirmModal.ts
@@ -558,6 +558,7 @@ export class ExportModal extends Modal {
         .writeText(value)
         .then(() => {
           new Notice(t("notice.fenCopied"));
+          return undefined;
         })
         .catch(() => {});
     });
