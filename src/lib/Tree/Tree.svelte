33c44b4f068dd41d07a54a5c5ab681b06bda0dd1
weshell <weshell@foxmail.com>
2026-08-14 10:08:54 +0800

    fix: remove unused helper functions in Tree.svelte

--- src/lib/Tree/Tree.svelte
+++ src/lib/Tree/Tree.svelte
@@ -116,20 +116,8 @@
     return node.annotation;
   }
 
-  function getAllAnnotations(node: ChessNode): string[] {
-    return node.annotation ? [node.annotation] : [];
-  }
-
-  const SHAPES_RE = /^([a-h][1-8])([a-h][1-8])?:([gryb])$/;
-
   function getRegularComments(node: ChessNode): string[] {
     return node.comments ?? [];
-  }
-
-  function getAllShapes(node: ChessNode): string[] {
-    return (
-      node.shapes?.map((s) => s.orig + (s.dest ?? "") + ":" + s.brush) ?? []
-    );
   }
 
   // ---- 自动保存逻辑 ----
